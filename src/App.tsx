e9d74b7d6ad28eb5572a301300f4f1a1f6828590
최수지 <pd0a7356@posco.com>
2022-09-20 16:47:55 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Routes from './Routes';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
+			<Routes />
+			{/* <header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
@@ -17,7 +18,7 @@ function App() {
 				>
 					app.tp
 				</a>
-			</header>
+			</header> */}
 		</div>
 	);
 }
---
+++ src/Routes.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// import JsonServerTest from './JsonServerTest';
+// import ReduxTest from './ReduxTest';
+import Button from '@mui/material/Button';
+
+function Home() {return <div>Hello World</div>;};
+
+function RoutesFunction(){
+	return (
+		<BrowserRouter 
+		// basename={initial}
+		> 
+			<Button href={'https://mui.com/'} >mui</Button>
+			<Button href={'https://popolChoi.github.io/app.tp'}  >popolChoi.github.io/app.tp</Button>
+
+			
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/proxy_test">프록시테스트</Link></li>
+				<li><Link to="/redux_test">redux test</Link></li>
+
+			</ul>
+
+
+			<Routes>
+				<Route path="/" element={<Home />} />
+				{/* <Route path="/proxy_test" element={<JsonServerTest />} />
+				<Route path="/redux_test" element={<ReduxTest />} /> */}
+
+			</Routes>
+		</BrowserRouter> 
+	
+            
+	) 
+}
+
+export default RoutesFunction;
+
